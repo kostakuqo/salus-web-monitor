e9c00716dee21ed393f40efc98e820701d13b553
kostakuqo <kostakuqo5@gmail.com>
2026-03-11 22:57:07 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/salus-web-monitor/',  // numele exact al repository-ului tău
   plugins: [react()],
 })
